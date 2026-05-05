e7891cb521ed1fb27a46fc40a0b0827decfd17d6
github-action-benchmark <github@users.noreply.github.com>
2026-05-05 01:32:23 +0000

    add Besu EVM JMH Benchmarks (jmh) benchmark result for 03495ab89c23571a4914dab7151cc26262f6664c

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777944743452,
+  "repoUrl": "https://github.com/parthdagia05/besu",
+  "entries": {
+    "Besu EVM JMH Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Parth Dagia",
+            "username": "parthdagia05",
+            "email": "parth.24bcs10414@sst.scaler.com"
+          },
+          "committer": {
+            "name": "Parth Dagia",
+            "username": "parthdagia05",
+            "email": "parth.24bcs10414@sst.scaler.com"
+          },
+          "id": "03495ab89c23571a4914dab7151cc26262f6664c",
+          "message": "Document benchmark workflow in docs/benchmarks/README.md\n\nSigned-off-by: Parth Dagia <parth.24bcs10414@sst.scaler.com>",
+          "timestamp": "2026-05-05T00:59:44Z",
+          "url": "https://github.com/parthdagia05/besu/commit/03495ab89c23571a4914dab7151cc26262f6664c"
+        },
+        "date": 1777944742488,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.hyperledger.besu.ethereum.vm.operations.AddOperationBenchmark.executeOperation ( {\"caseName\":\"ADD_RANDOM_RANDOM\"} )",
+            "value": 89.62256907066242,
+            "unit": "ns/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.hyperledger.besu.ethereum.vm.operations.v2.AddOperationBenchmarkV2.executeOperation ( {\"caseName\":\"ADD_RANDOM_RANDOM\"} )",
+            "value": 12.793001445953518,
+            "unit": "ns/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
